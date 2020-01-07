adc1bdbe02b928ead274aab766333f38b77841f3
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2020-01-07 17:48:32 +0100

    Added blank projects for forntend and backend

---
+++ Upp-Backend/src/main/java/com/upp/nc/NcApplication.java
@@ -0,0 +1,13 @@
+package com.upp.nc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcApplication.class, args);
+    }
+
+}
